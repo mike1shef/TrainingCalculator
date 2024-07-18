5b2d50d436790dfb1203e3f0fb44650d18c8f625
mikhailshauchenka <mike1shef@gmail.com>
2024-07-18 12:45:24 +0200

    - Room DB fix on real device.

--- composeApp/src/iosMain/kotlin/database/DBClient.ios.kt
+++ composeApp/src/iosMain/kotlin/database/DBClient.ios.kt
@@ -2,17 +2,37 @@ package database
 
 import androidx.room.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
-import platform.Foundation.NSHomeDirectory
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import platform.Foundation.*
 
 actual class DBClient {
     actual val database: TrainingCalculatorDatabase = getDatabase()
 
     private fun getDatabase(): TrainingCalculatorDatabase {
-        val dbFilePath = NSHomeDirectory() + "/my_room.db"
+        val dbFilePath = "${fileDirectory()}/my_room.db"
+
         return Room.databaseBuilder<TrainingCalculatorDatabase>(
             name = dbFilePath,
             factory =  { TrainingCalculatorDatabase::class.instantiateImpl() }
-            ).setDriver(BundledSQLiteDriver())
+            )
+            .addMigrations()
+            .fallbackToDestructiveMigrationOnDowngrade(true)
+            .setDriver(BundledSQLiteDriver())
+            .setQueryCoroutineContext(Dispatchers.IO)
                 .build()
         }
     }
+
+@OptIn(ExperimentalForeignApi::class)
+private fun fileDirectory(): String {
+    val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory).path!!
+}
